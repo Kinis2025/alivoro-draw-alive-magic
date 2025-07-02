88ae3cbe3fe9a364309cd78169cbad9c0292e42f
Kinis2025 <olegssmirnovs89@gmail.com>
2025-07-02 11:49:54 +0300

    Create ProtectedRoute.tsx

---
+++ src/components/ProtectedRoute.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../firebase";
+
+const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
+  const [user, loading, error] = useAuthState(auth);
+
+  if (loading) return <div>Loading...</div>;
+  if (!user) return <Navigate to="/login" replace />;
+  return children;
+};
+
+export default ProtectedRoute;
