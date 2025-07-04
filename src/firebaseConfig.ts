6d2361fbbb0ea04d2f2d6d8319ee31829d6013aa
Kinis2025 <olegssmirnovs89@gmail.com>
2025-07-04 13:48:20 +0300

    Update firebaseConfig.ts

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,16 +1,19 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCxVKrh9Lf_0Kv71P-aFUSMNPjGTfVNOP0",
-  authDomain: "alivoro.firebaseapp.com",
-  projectId: "alivoro",
-  storageBucket: "alivoro.appspot.com",
-  messagingSenderId: "176947871728",
-  appId: "1:176947871728:web:6aef69d79b960da2cda735",
-  measurementId: "G-HV62PYSRDX",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export default app;
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { app, auth, db };
