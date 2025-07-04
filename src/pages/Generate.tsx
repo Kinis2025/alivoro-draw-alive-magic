de661275c307ecc9e891e8010332d6faf2a020df
Kinis2025 <olegssmirnovs89@gmail.com>
2025-07-04 10:47:02 +0300

    Create Generate.tsx

---
+++ src/pages/Generate.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Generate = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Generate Video Page</h1>
+      {/* Šeit ieliksi savu UploadSection komponenti vai citu ģenerācijas rīku */}
+    </div>
+  );
+};
+
+export default Generate;
